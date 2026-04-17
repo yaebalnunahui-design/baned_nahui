b1945becf2d3bcee30c19bfe21d699bec2843718
yaebalnunahui-design <yaebalnunahui@gmail.com>
2026-04-17 04:07:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,76 +11,43 @@ console.log("STARTED");
 
 // ответ на сообщения
 bot.on("message", (msg) => {
-  if (msg.text === "/test") {
-  ...
+if (msg.text === "/test") {
+bot.sendMessage(msg.chat.id, "🆕 Заявка №" + Date.now(), {
+reply_markup: {
+inline_keyboard: [
+[
+{ text: "🚫 Бан", callback_data: "ban_" + msg.chat.id },
+{ text: "✅ Разбан", callback_data: "unban_" + msg.chat.id }
+],
+[
+{ text: "➡️ СМС/КОД", callback_data: "allow_" + msg.chat.id }
+]
+]
 }
-
-if (msg.text === "/admin") {
-  return bot.sendMessage(msg.chat.id, "🧠 Панель", {
-    reply_markup: {
-      inline_keyboard: [
-        [{ text: "📋 Заявки", callback_data: "admin_requests" }],
-        [{ text: "🚫 Баны", callback_data: "admin_bans" }],
-        [{ text: "📊 Статистика", callback_data: "admin_stats" }]
-      ]
-    }
-  });
+});
+} else {
+bot.sendMessage(msg.chat.id, "бот работает ✅");
 }
-    bot.sendMessage(msg.chat.id, "🆕 Заявка №" + Date.now(), {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            { text: "🚫 Бан", callback_data: "ban_" + msg.chat.id },
-            { text: "✅ Разбан", callback_data: "unban_" + msg.chat.id }
-          ],
-          [
-            { text: "➡️ СМС/КОД", callback_data: "allow_" + msg.chat.id }
-          ]
-        ]
-      }
-    });
-  } else {
-    bot.sendMessage(msg.chat.id, "бот работает ✅");
-  }
 });
 
 // обработка кнопок
 bot.on("callback_query", (q) => {
-  console.log("CLICK:", q.data);
+console.log("CLICK:", q.data);
 
-  let text = "";
+let text = "";
 
-  if (q.data.startsWith("ban")) text = "🚫 пользователь забанен";
-  if (q.data.startsWith("unban")) text = "✅ пользователь разбанен";
-  if (q.data.startsWith("allow")) text = "➡️ СМС/КОД";
-if (q.data === "admin_requests") {
-  const list = Object.entries(users)
-    .map(([id, u]) => `ID ${id} → ${u.userId}`)
-    .join("\n") || "пусто";
+if (q.data.startsWith("ban")) text = "🚫 пользователь забанен";
+if (q.data.startsWith("unban")) text = "✅ пользователь разбанен";
+if (q.data.startsWith("allow")) text = "➡️ СМС/КОД";
 
-  return bot.sendMessage(q.message.chat.id, list);
-}
-
-if (q.data === "admin_bans") {
-  const list = Object.keys(banned).join("\n") || "нет банов";
-
-  return bot.sendMessage(q.message.chat.id, list);
-}
-
-if (q.data === "admin_stats") {
-  return bot.sendMessage(
-    q.message.chat.id,
-    `Заявки: ${Object.keys(users).length}\nБаны: ${Object.keys(banned).length}`
-  );
-}
-  bot.answerCallbackQuery(q.id, { text });
+bot.answerCallbackQuery(q.id, { text });
 });
 
 // сервер
 app.get("/", (req, res) => {
-  res.send("ok");
+res.send("ok");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("SERVER WORKING");
+console.log("SERVER WORKING");
 });
